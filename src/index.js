f3e5408f680ab85e3f8eb180bf85388203c8b286
Ivan Tarasov <guser999@mail.ru>
2020-02-05 17:49:08 +0700

    3. Board переписал на генерацию через вложенные циклы, вместо хардкода

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                key={i}
             />
         );
     }
@@ -23,21 +24,11 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {[0, 1, 2].map(index => (
+                    <div className="board-row" key={index}>
+                        {[0, 1, 2].map(value => this.renderSquare(index * 3 + value))}
+                    </div>
+                ))}
             </div>
         );
     }
